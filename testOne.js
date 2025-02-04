f8181a95369ebaa6dac867d33c5aa8d7669948c6
Niketan <wadaskarniketan@gmail.com>
2025-02-04 20:33:09 +0530

    daily push

--- testOne.js
+++ testOne.js
@@ -1,31 +1,31 @@
-const points = [25, 40, 1, 5, 100, 10];
-function findSecondLargest(arr) {
-  let largest = -Infinity;
-  let secondLargest = -Infinity;
+// Selection sort
+// bubble sort
 
-  arr.map((num, index) => {
-    if(num > largest) {
-      secondLargest = largest;
-      largest = num;
-    } else if (num > secondLargest && num !== largest) {
-        secondLargest = num;
-    }
-});
-console.log(largest , secondLargest, "");
-  // ----------------
-  //   let max = -Infinity;
-  //   let secondMax = -Infinity;
+const arr = [5, 3, 7, 9, 1, 4, 79, 3, 6, 8, 9, 3, 6, 8, 9, 21, 0];
+// function bubbleSort(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr.length - 1 - i; j++) {
+//       if (arr[j] > arr[j + 1]) {
+//         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+//       }
+//     }
+//   }
+//   return arr;
+// }
 
-  //   for (let i = 0; i < arr.length; i++) {
-  //     if (arr[i] > max) {
-  //       secondMax = max;
-  //       max = arr[i];
-  //     } else if (arr[i] > secondMax && arr[i] !== max) {
-  //       secondMax = arr[i];
-  //     }
-  //   }
+// function selectionSort(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     let minIndex = i;
+//     for (let j = i; j < arr.length; j++) {
+//       if (arr[minIndex] > arr[j]) {
+//         minIndex = j;
+//       }
+//     }
+//     if (arr[minIndex] < arr[i]) {
+//       [ arr[i],arr[minIndex]] = [ arr[minIndex],arr[i]];
+//     }
+//   }
+//   return arr;
+// }
 
-  //   console.log(secondMax);
-}
-
-findSecondLargest(points);
+// console.log(selectionSort(arr));
