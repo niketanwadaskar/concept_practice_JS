e126145dbb7a55ec89a9a392a2ece87c1d4e50bc
Niketan <wadaskarniketan@gmail.com>
2025-02-05 21:47:07 +0530

    binary and linear search

--- FandaMentals.js
+++ FandaMentals.js
@@ -1,6 +1,8 @@
 // //? Selection Sort; by brute force method;
 
 const arr = [3, 5, 1, 8, 2, 9, 6, 7, 4, 0];
+const sortedArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
 // function selectionSort(arr) {
 //   for (let i = 0; i < arr.length; i++) {
 //     let minIndex = i; // consider min is first
@@ -28,4 +30,51 @@ const arr = [3, 5, 1, 8, 2, 9, 6, 7, 4, 0];
 // return arr;
 // }
 
-console.log(bubbleSort(arr));
+// console.log(insertionSort(arr));
+
+// //?Insertion Sort // brute force
+
+// function insertionSort(arr) {
+//   for (let i = 1; i < arr.length; i++) {
+//     let key = arr[i];
+//     let j = i - 1;
+//     while (j >= 0 && arr[j] > key) {
+//       arr[j + 1] = arr[j];
+//       j--
+//     }
+//     arr[j + 1] = key;
+//   }
+//   return arr;
+// }
+
+//? Linear Search
+
+function linearSearch(arr, k) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == k) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+//? Binary Search
+
+function binarySearch(arr, k) {
+  let n = arr.length;
+  let left = 0;
+  let right = n - 1;
+  while (left <= right) {
+    let mid =left + Math.round((right-left ) / 2);
+    if (arr[mid] == k) {
+      return mid;
+    } else if (arr[mid] < k) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return -1;
+}
+
+console.log(binarySearch(sortedArr, 83));
