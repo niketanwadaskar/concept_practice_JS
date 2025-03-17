8da1a1e0f8c0669cb1a3dc1f834dc6180c7a0e71
Niketan <niketanw@mirakitech>
2025-03-17 12:41:22 +0530

    test config

---
+++ testOne.js
@@ -0,0 +1,11 @@
+const str = "hello";
+
+function reverseString(str) {
+  let reversed = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed = reversed + str[i];
+  }
+  return reversed;
+}
+
+console.log(reverseString(str));
